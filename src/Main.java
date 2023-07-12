587fdd7e6fba855ab0a1a02288de2c1c52537902
GenoWeb <evilbrain.exe@gmail.com>
2023-07-12 14:48:21 +0200

    Init Project

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame newFrame = new JFrame("Test");
+        newFrame.setSize(300, 300);
+        newFrame.setVisible(true);
+    }
+}
